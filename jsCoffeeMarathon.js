7df0e20afe473d2d6cc6a532b5e47e4f04790f5a
Jamie Thompson <jlthompson76@users.noreply.github.com>
2020-03-04 15:25:56 -0500

    Add files via upload

---
+++ jsCoffeeMarathon.js
@@ -0,0 +1,41 @@
+console.log("Coffee Please!");
+
+let coffeeType = "Medium Roast";
+
+console.log(coffeeType);
+
+// var latte = 2;
+// var mocha = 3;
+
+// console.log(latte + mocha);
+
+// console.log(latte > mocha);
+
+// console.log(latte < mocha);
+
+// let sugarFree;
+
+// alert(sugarFree);
+
+// let customer1 = "Kamala Khan", drinkChoice = "chai", quantity = 4, foodAllergy = false;
+
+// console.log(customer1 + " " + drinkChoice + " " + quantity + " " + foodAllergy);
+
+let chai = 2.50, quantity = 4, drinkChoice = "chai"; // changed const chai = 2.50 to let chai = 2.50
+
+
+console.log(chai * quantity);
+
+// chai = 2;
+// var chai = 2;
+
+console.log(chai);
+
+console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++");
+let total = "$" + quantity * chai;
+alert(total);
+console.log("+ Your final order is " + quantity + " " + drinkChoice + "s, for a total of " + total + ". +");
+
+let soda = "Sprite";
+console.log("+ Nice choice of " + drinkChoice + ", but we recommend a " + soda + "!  + ");
+console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++");
